214d8f423e1d4c7ff6879e26cbc3519bc80cc80c
Diego Fernandes <diego.schell.f@gmail.com>
2021-01-19 15:34:31 -0300

    Configurando estilos globais

---
+++ src/styles/global.ts
@@ -0,0 +1,47 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #f0f2f5;
+    --red: #E52E4D;
+    --blue: #5429CC;
+
+    --blue-light: #6933FF;
+
+    --text-title: #363F5F;
+    --text-body: #969CB3;
+
+    --background: #F0F2F5;
+    --shape: #FFFFFF;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%; 
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`
