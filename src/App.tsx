01a1afa37181f138084fe18546860491bf9202b5
Diego Fernandes <diego.schell.f@gmail.com>
2021-01-19 16:41:49 -0300

    Componente: Summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
   :root {
     --background: #f0f2f5;
     --red: #E52E4D;
+    --green: #33CC95;
     --blue: #5429CC;
 
     --blue-light: #6933FF;
